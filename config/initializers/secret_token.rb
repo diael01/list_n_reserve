76267c28d10e4bf24a8e471dadbbf8f6148ed730
neuron1 <neuron1@neuron1s-MacBook-Air.local>
2013-11-16 22:11:47 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SportsReservations::Application.config.secret_key_base = 'c4035c5a7a3b0c5c1998630c10f18690603e27f6023d01ee4dc38c4f8f2ab5207f1d0817088a60d3e4961f93eb02e0635e8d48d929220ce4b1a2e547d54267cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SportsReservations::Application.config.session_store :cookie_store, key: '_SportsReservations_session'
